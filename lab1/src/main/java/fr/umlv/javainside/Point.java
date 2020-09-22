5f5e0ec62e87d3a75a48031e455ccb3916fe8256
Romain Grué <romain.grue98@gmail.com>
2020-09-22 10:35:51 +0200

    ajout pom et travis

---
+++ lab1/src/main/java/fr/umlv/javainside/Point.java
@@ -0,0 +1,5 @@
+package fr.umlv.javainside;
+
+public record Point(int x, int y){
+    
+}
---
+++ lab1/src/test/java/fr/umlv/javainside/PointTest.java
@@ -0,0 +1,21 @@
+package fr.umlv.javainside;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PointTest {
+    private Point point = new Point(5,3);
+
+    @Test
+    public void shouldReturnRightX() {
+        assertEquals(point.x(), 5);
+    }
+
+    @Test
+    public void shouldReturnRightY() {
+        assertEquals(point.y(), 3);
+    }
+
+
+}
